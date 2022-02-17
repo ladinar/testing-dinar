0a3317e7e4e714649313ecde1c8d0f58beb759c9
Ladinar <ladinarnanda@gmail.com>
2022-02-17 15:25:52 +0700

    update authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,19 +4,61 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
+use Hash;
+use App\Http\Requests\RegisterRequest;
+use \Exception;
+use Illuminate\Http\Response;
 
 class AuthController extends Controller
 {
     //
-    public function login(Request $request){
-        if(Auth::attempt($request->only('email','password'))){
-            $user = Auth::user();
-
-            return $user;
-        }
+    public function login(Request $request)
+    {
+        try {
+            if(Auth::attempt($request->only('email','password'))){
+                $user = Auth::user();
+                $token = $user->createToken('app')->accessToken;
+    
+                return response([
+                    'message' => 'success', 
+                    'token' => $token,
+                    'user' => $user
+                ]);
+            }
+            
+        } catch (\Exception $exception){
+            return response([
+                'message' => $exception->getMessage()
+            ], 400);
+        }        
 
         return response([
             'message' => 'invalid username/password'
         ], 401);
+        
+    }
+
+    public function user()
+    {
+        return Auth::user();
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        try {
+            $user = User::create([
+                'username' => $request->input('username'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password')),
+            ]);
+
+            return $user;
+        } catch (Exception $exception){
+            return response ([
+                'message' => $exception->getMessage()
+            ], 400);
+        }       
+
     }
 }
